54226b4b7bdf7152c30a1b60408b625adb7c7e1a
ananayap12345-debug <ananaya.p12345@gmail.com>
2026-04-17 16:44:36 +0530

    dgr

--- app.py
+++ app.py
@@ -9,11 +9,11 @@ fare=st.number_input("Enter your fare",0,500)
 embarked=st.radio("select your boarding station",["s","c","Q"])
 user_input=[[pclass,gender,age,sibsp,parch,fare,embarked]]
 
-import joblib
-pipe = joblib.load("pipe1.pkl")
-# import pickle
-# f=open("pipe1.pkl","rb")
-# pipe=pickle.load(f)
+# import joblib
+# pipe = joblib.load("pipe1.pkl")
+import pickle
+f=open("pipe1.pkl","rb")
+pipe=pickle.load(f)
 if st.button("click to predict "):
     res=pipe.predict(user_input)
     st.write(res)
